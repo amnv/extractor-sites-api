1c55b58ee30689395e41ec6c2b7e8254d35ddd07
allyson <allyson.venceslau@gmail.com>
2021-01-27 17:00:24 -0300

    Feature: First commit. Spring boot project

---
+++ src/main/java/com/personal/extractorsitesapi/ExtractorSitesApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.extractorsitesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractorSitesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractorSitesApiApplication.class, args);
+	}
+
+}
