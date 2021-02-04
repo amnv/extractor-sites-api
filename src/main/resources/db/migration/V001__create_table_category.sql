f64d0c9798169c43bb396512ae2e941141a24891
allyson <allyson.venceslau@gmail.com>
2021-02-04 17:06:13 -0300

    feature: Adding sequence to create table category

--- src/main/resources/db/migration/V001__create_table_category.sql
+++ src/main/resources/db/migration/V001__create_table_category.sql
@@ -1,13 +1,20 @@
+CREATE SEQUENCE public.code_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
 CREATE TABLE category (
-    code BIGINT PRIMARY KEY NOT NULL DEFAULT nextval('code_id_seq'::regclass),
+    code BIGINT NOT NULL DEFAULT nextval('code_id_seq'::regclass),
     nome VARCHAR(50) NOT NULL,
 
     CONSTRAINT code_pkey PRIMARY KEY (code)
     );
 
 
-    INSERT INTO category values ('Lazer');
-    INSERT INTO category values ('Alimentação');
-    INSERT INTO category values ('Supermercado');
-    INSERT INTO category values ('Farmácia');
-    INSERT INTO category values ('Outros');
+INSERT INTO category values (1, 'Lazer');
+INSERT INTO category values (2, 'Alimentação');
+INSERT INTO category values (3, 'Supermercado');
+INSERT INTO category values (4, 'Farmácia');
+INSERT INTO category values (5, 'Outros');
