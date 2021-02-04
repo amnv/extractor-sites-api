b275b9ca7ea2a53720ebf2cc3906144891d203ac
allyson <allyson.venceslau@gmail.com>
2021-02-04 17:50:18 -0300

    feature: Defening return to create category method

--- src/main/java/com/personal/extractorsitesapi/resourcer/CategoryResource.java
+++ src/main/java/com/personal/extractorsitesapi/resourcer/CategoryResource.java
@@ -4,6 +4,7 @@ import com.personal.extractorsitesapi.model.Category;
 import com.personal.extractorsitesapi.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,12 +25,13 @@ public class CategoryResource {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody Category category, HttpServletResponse response) {
+    public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
         Category categorySaved = this.categoryRepository.save(category);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequestUri().path("/{code}")
                 .buildAndExpand(categorySaved.getCode()).toUri();
         response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categorySaved);
     }
 }
