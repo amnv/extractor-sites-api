623566d4f9ad5e3530d48e89025a389c787ac0f9
allyson <allyson.venceslau@gmail.com>
2021-02-04 18:10:30 -0300

    fix: Correcting equals and hash code class category

--- src/main/java/com/personal/extractorsitesapi/model/Category.java
+++ src/main/java/com/personal/extractorsitesapi/model/Category.java
@@ -28,16 +28,29 @@ public class Category {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return Objects.equals(code, category.code);
-    }
 
     @Override
     public int hashCode() {
-        return Objects.hash(code);
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((code == null) ? 0 : code.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Category other = (Category) obj;
+        if (code == null) {
+            if (other.code != null)
+                return false;
+        } else if (!code.equals(other.code))
+            return false;
+        return true;
     }
 }
