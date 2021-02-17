3db9375c3cf844f95699aa0e8c510a53a29854d9
allyson <allyson.venceslau@gmail.com>
2021-02-17 18:18:35 -0300

    feature: Add personalised text to EmptyResultDataAccessException

--- src/main/java/com/personal/extractorsitesapi/exceptionHandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/personal/extractorsitesapi/exceptionHandler/AlgamoneyExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -44,6 +46,13 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler{
                     fieldError.toString()));
         }
         return errors;
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    protected ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
+        String userMessage = messageSource.getMessage("message.not_found", null, LocaleContextHolder.getLocale());
+        String devMessage = ex.getMessage();
+        return handleExceptionInternal(ex, new Error(userMessage, devMessage), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     public static class Error {
