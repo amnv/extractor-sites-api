c1d6587c250da32cbc8d6f04c22be294e23289c6
Allyson Nascimento <a.venceslau@sidi.org.br>
2021-03-11 15:20:01 -0300

    Adding class FinanceReleases and Enum Type

---
+++ src/main/java/com/personal/extractorsitesapi/model/FinanceReleases.java
@@ -0,0 +1,47 @@
+package com.personal.extractorsitesapi.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Finance_releases {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
+
+    @NotNull
+    private String description;
+
+    @Column(name = "due_date")
+    @NotNull
+    private LocalDate dueDate;
+
+    @Column(name = "pay_date")
+    private LocalDate payDate;
+
+    @NotNull
+    private BigDecimal amount;
+
+    private String obs;
+
+    @NotNull
+    private Type type;
+
+    @ManyToOne
+    @JoinColumn(name = "category_fk")
+    @NotNull
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "person_fk")
+    @NotNull
+    private Person person;
+}
---
+++ src/main/java/com/personal/extractorsitesapi/model/Type.java
@@ -0,0 +1,5 @@
+package com.personal.extractorsitesapi.model;
+
+public enum Type {
+    income, expenses;
+}
